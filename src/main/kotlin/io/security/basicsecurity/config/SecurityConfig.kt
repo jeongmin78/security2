d16560bb8e58e19a6c72f73671fe9baf7b9df3ea
jeongmin78 <wudals0708@naver.com>
2023-01-19 23:31:37 +0900

    feat: rememberMe 기능 추가

--- src/main/kotlin/io/security/basicsecurity/config/SecurityConfig.kt
+++ src/main/kotlin/io/security/basicsecurity/config/SecurityConfig.kt
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val userDetailsService: UserDetailsService
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity) : SecurityFilterChain {
@@ -42,6 +45,15 @@ class SecurityConfig {
             it.logoutSuccessHandler { request, response, authentication ->
                 response.sendRedirect("/login")
             }
+        }
+
+        //세션이 만료되고 웹브라우저가 종료된 후에도 어플리케이션이 사용자를 기억하는 기능
+        http.rememberMe {
+            println("remember-me")
+            it.rememberMeParameter("remember") // 기본 파라미터명은 remember-me
+            it.tokenValiditySeconds(3600) // default 14일
+            // it.alwaysRemember(true) // 로그인시 무조건 리멤버미 기능 활성화
+            it.userDetailsService(userDetailsService) // 내부적으로 재인증 처리를 위해서 필요함
         }
 
         return http.build()
